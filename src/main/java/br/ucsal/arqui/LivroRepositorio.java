01e0771f9d4725bd48fb5820743ede5d60febb74
LuccaBN <lbnyg.ti@gmail.com>
2024-06-11 22:48:05 -0300

    subir Getway

--- src/main/java/br/ucsal/arqui/LivroRepositorio.java
+++
@@ -1,9 +0,0 @@
-package br.ucsal.arqui;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface LivroRepositorio extends JpaRepository<Livro, Long> {
-        List<Livro> findByDisponivel(boolean disponivel);
-}
-
