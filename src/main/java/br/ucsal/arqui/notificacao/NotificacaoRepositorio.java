49fc74ba63dc4afdd06106c15e630d0cf7bf58d2
LuccaBN <lbnyg.ti@gmail.com>
2024-06-12 23:19:06 -0300

    notificacao completa

--- src/main/java/br/ucsal/arqui/notificacao/NotificacaoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package br.ucsal.arqui.notificacao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface NotificacaoRepositorio extends JpaRepository<Notificacao, Long> {
-    List<Notificacao> findByUserId(Long usuarioId);
-}
